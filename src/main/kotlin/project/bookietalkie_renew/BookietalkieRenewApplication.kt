8478165d27b7c74c0131c3db331f62679e178d77
Shinye Song <shinye.song@croquis.com>
2020-09-06 17:48:05 +0900

    initial commit

---
+++ src/main/kotlin/project/bookietalkie_renew/BookietalkieRenewApplication.kt
@@ -0,0 +1,11 @@
+package project.bookietalkie_renew
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BookietalkieRenewApplication
+
+fun main(args: Array<String>) {
+	runApplication<BookietalkieRenewApplication>(*args)
+}
---
+++ src/test/kotlin/project/bookietalkie_renew/BookietalkieRenewApplicationTests.kt
@@ -0,0 +1,13 @@
+package project.bookietalkie_renew
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookietalkieRenewApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
