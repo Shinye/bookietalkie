bdc52e4c5b148916274f156bbfb3bb9ac4a794b2
Shinye Song <shinye.song@croquis.com>
2020-11-29 22:04:19 +0900

    wip

---
+++ src/main/kotlin/project/bookietalkie_renew/api/v1/BookController.kt
@@ -0,0 +1,8 @@
+package project.bookietalkie_renew.api.v1
+
+import org.springframework.stereotype.Controller
+
+@Controller
+class BookController {
+
+}
---
+++ src/main/kotlin/project/bookietalkie_renew/config/Config.kt
@@ -0,0 +1,11 @@
+package project.bookietalkie_renew.config
+
+import project.bookietalkie_renew.domain.user.UserRepository
+import project.bookietalkie_renew.domain.user.UserService
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class Config {
+
+}
